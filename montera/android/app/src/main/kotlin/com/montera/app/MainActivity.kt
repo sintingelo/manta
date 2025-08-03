01a1dd84bc97f23db04617393ae72b061d150e7c
sintingelo <sintingelo@gmail.com>
2025-08-03 08:16:03 +0700

    Delete montera directory

--- montera/android/app/src/main/kotlin/com/montera/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.montera
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- montera/lib/models/watchlist_model.dart
+++
@@ -1,7 +0,0 @@
-// lib/models/watchlist_model.dart
-class WatchlistModel {
-  final String symbol;
-  final String name;
-
-  WatchlistModel({required this.symbol, required this.name});
-}
